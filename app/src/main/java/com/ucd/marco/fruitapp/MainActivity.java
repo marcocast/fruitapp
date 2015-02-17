aa4f5a4ac726165d83125d500a3f3b9910ef4bdc
Castigliego, Marco <marco.castigliego@teamaol.com>
2015-02-17 12:39:59 +0000

    second commit

--- app/src/main/java/com/ucd/marco/fruitapp/MainActivity.java
+++ app/src/main/java/com/ucd/marco/fruitapp/MainActivity.java
@@ -1,17 +1,69 @@
 package com.ucd.marco.fruitapp;
+        import android.content.Intent;
+        import android.os.Bundle;
+        import android.view.View;
+        import android.widget.AdapterView;
+        import android.widget.ListView;
+        import android.widget.Toast;
+        import android.app.Activity;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.ImageView;
+        import java.util.Collections;
+        import java.util.HashMap;
+        import java.util.Map;
+        import java.util.Random;
 
-
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     public final static String EXTRA_FRUIT_NAME = "com.ucd.marco.fruitapp.FRUIT_NAME";
+    private Random random = new Random();
+    private ListView list;
+    private String[] fruits = {
+            "Kiwi",
+            "Dragon",
+            "Banana"
+    } ;
+    private Integer[] imageId = {
+            R.drawable.kiwi,
+            R.drawable.dragon,
+            R.drawable.banana
+    };
+
+    private static String[] kiwiFacts = {
+            "Kiwifruit has also been considered a nutritional all-star",
+            "kiwifruit has the best nutrient density of 21 commonly consumed fruits",
+            "If you want clean energy, think of kiwifruit because they’re rich in magnesium",
+            "Kiwifruit has been growing in New Zealand for over 100 years",
+            "At first, kiwis were referred to as Yang Tao or Chinese Gooseberry"
+    } ;
+
+    private static String[] dragonFacts = {
+            "To choose a ripe dragon fruit: look for bright, even-colored skin",
+            "Dragon fruit is loaded with beneficial antioxidant",
+            "Dragon fruit is also called Pitaya",
+            "Dragon fruit does have a small amount of fats because there are so many seeds in the edible part of the fruit."
+    } ;
+
+
+    private static String[] bananaFacts = {
+            "Bananas are the fruit produced by various banana plants",
+            "When rip they are usually long and curved with a soft inside covered by a yellow skin (peel)",
+            "Bananas can be found in other colors, including red",
+            "Banana plants are not trees, they are a type of herb",
+            "Bananas have high nutritional value and are a healthy snack",
+            "Humans have grown bananas for thousands of years"
+    } ;
+
+
+   private static Map<String,String[]> fruitFacts = new HashMap<String,String[]>();
+    static {
+        Map<String,String[]> tempFruitFacts = new HashMap<String,String[]>();
+        tempFruitFacts.put("Kiwi", kiwiFacts);
+        tempFruitFacts.put("Dragon", dragonFacts);
+        tempFruitFacts.put("Banana", bananaFacts);
+        fruitFacts = Collections.unmodifiableMap(Collections.unmodifiableMap(tempFruitFacts));
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,38 +71,24 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         final Intent intent = new Intent(this, FruitDetailsActivity.class);
+        SingleFruitList adapter = new SingleFruitList(MainActivity.this, fruits, imageId);
+        list=(ListView)findViewById(R.id.list);
+        list.setAdapter(adapter);
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
 
-
-
-        ImageView imgKiwi = (ImageView) findViewById(R.id.imageKiwi);
-        imgKiwi.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                intent.putExtra(EXTRA_FRUIT_NAME, "kiwi");
-
+                Toast.makeText(MainActivity.this, getFruitFact(fruits[+ position]), Toast.LENGTH_LONG).show();
+                intent.putExtra(EXTRA_FRUIT_NAME, fruits[+ position]);
                 startActivity(intent);
             }
         });
-
-        ImageView imgBanana = (ImageView) findViewById(R.id.imageBanana);
-        imgBanana.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                intent.putExtra(EXTRA_FRUIT_NAME, "banana");
-
-                startActivity(intent);
-            }
-        });
-
-        ImageView imgDragon = (ImageView) findViewById(R.id.imageDragon);
-        imgDragon.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                intent.putExtra(EXTRA_FRUIT_NAME, "dragon");
-
-                startActivity(intent);
-            }
-        });
-
-
     }
 
 
+    private String getFruitFact(String fruit){
+        String[] facts =  fruitFacts.get(fruit);
+        int randomIndex = random.nextInt(facts.length - 0 + 1) + 0;
+        return facts[randomIndex];
+    }
 }
---
+++ app/src/main/java/com/ucd/marco/fruitapp/SingleFruitList.java
@@ -0,0 +1,29 @@
+package com.ucd.marco.fruitapp;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+public class SingleFruitList extends ArrayAdapter<String> {
+    private final Activity context;
+    private final String[] fruits;
+    private final Integer[] imageId;
+    public SingleFruitList(Activity context,
+                           String[] fruits, Integer[] imageId) {
+        super(context, R.layout.list_single_fruit, fruits);
+        this.context = context;
+        this.fruits = fruits;
+        this.imageId = imageId;
+    }
+    @Override
+    public View getView(int position, View view, ViewGroup parent) {
+        LayoutInflater inflater = context.getLayoutInflater();
+        View rowView= inflater.inflate(R.layout.list_single_fruit, null, true);
+        ImageView imageView = (ImageView) rowView.findViewById(R.id.img);
+        imageView.setImageResource(imageId[position]);
+        return rowView;
+    }
+}
